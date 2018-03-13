a88cb13e22aafd70135158907ca90638d067d0fd
SethDonohue <donohue.seth@gmail.com>
2018-03-13 11:31:22 -0700

    added other solutions, corrected first solution alpha string

--- codewars/consonantValue.js
+++ codewars/consonantValue.js
@@ -1,29 +1,39 @@
 function solve(str) {
-
   // build map for letter values
   const map = {};
-  const alphabet = 'abcdefghijklmnopqrstuvwyxz';
-  for( let i = 0; i < 26; i++) {
-     map[alphabet[i]] = i+1;
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  for (let i = 0; i < 26; i++) {
+    map[alphabet[i]] = i + 1;
   }
   
   // take str and split into array via vowels, regex
-  let subStrArr = str.split(/[aeiou]/);
+  const subStrArr = str.split(/[aeiou]/);
   console.log(subStrArr);
   // go through array, add values up for each element (now a substr)
   let max = 0;
-  subStrArr.forEach( element => {
+  subStrArr.forEach(element => {
     let temp = 0;
-    for( let char of element) {
-      if(map[char]){
+    for (const char of element) {
+      if (map[char]) {
         temp += map[char];
       }      
     }
-    if(temp > max) max = temp;
+    if (temp > max) max = temp;
     console.log(max);
-  })
+  });
   
-    // if substr value i shigher than max set it to max
+  // if substr value i shigher than max set it to max
     
   return max;
+}
+
+// other solutions
+solve = s => s.split(/[aeiou]+/).reduce((s, n) => Math.max(s, n.split('').reduce((a, b) => a + b.charCodeAt(0) - 96, 0)), 0);
+
+const solve = (s) => {
+  return Math.max(...s.split(/[aeiou]+/).map((seq) => {
+    return [...seq].reduce((r, c) => {
+      return r + c.charCodeAt(0) - 96;
+    }, 0);
+  }));
 };
