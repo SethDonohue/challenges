9acbee5f0231ecc664a7060c4a6301fdf95d9d24
SethDonohue <donohue.seth@gmail.com>
2018-03-18 22:12:37 -0700

    added correct solution and tests using odd/even height

--- codewars/snailArray.js
+++ codewars/snailArray.js
@@ -8,41 +8,52 @@
 // snail(array) #=> [1, 2, 3, 6, 9, 8, 7, 4, 5]
 
 const snail = (array) => {
+
+  // Check for empty input array
+  if (array.length === 1) return array[0];
+  if (array.length === 0) return [];
+  
+  // Check for NON square array
+  if (array.length !== array[0].length) return null;
+
   let rowTop = 0; // increment by 1 when finished with row
   let rowBottom = array.length - 1; // decrement by 1 when finished with row
   let columnBack = array[0].length - 1; // decrement by 1 when finsihed with column
   let columnFront = 0; // increment by 1 when finished with column
-  const height = array.length;
-  const width = array[0].length;
+  // const height = array.length;
+  // const width = array[0].length;
 
-  console.log(`Height:${height} Width:${width}`);
-  console.log(`RowTop:${rowTop} RowBottom:${rowBottom}...ColumnBack:${columnBack} ColumnStart:${columnFront}`);
+  let odd = true; // assume it is odd first
+  if (array.length % 2 === 0) odd = false; // array is even
+
+  // console.log(`Height:${height} Width:${width}`);
+  // console.log(`RowTop:${rowTop} RowBottom:${rowBottom}...ColumnBack:${columnBack} ColumnStart:${columnFront}`);
 
   const result = [];
   // const map = {};
 
   const _rowElementPusher = (currentRow, startColumn, endColumn) => {
-    console.log('============ RowPusher Called ============');
+    // console.log('============ RowPusher Called ============');
     // slice from original row the elements we need to push...
     let subArray = [];
     
-    if (endColumn < startColumn) { // slicing right to left, REVERSE order...
+    if (startColumn > endColumn) { // slicing right to left, REVERSE order...
       subArray = array[currentRow].slice(endColumn, startColumn + 1);      
       subArray.reverse();
-      console.log('subArray REVERSED: ', subArray);
+      // console.log('subArray REVERSED: ', subArray);
     } else {
       subArray = array[currentRow].slice(startColumn, endColumn + 1);      
-      console.log('subArray: ', subArray);
+      // console.log('subArray: ', subArray);
     }
     // ... and then push them to result array
     for (let i = 0; i < subArray.length; i++) {
       result.push(subArray[i]);
-      console.log('ROW Pushed Element: ', subArray[i]);
+      // console.log('ROW Pushed Element: ', subArray[i]);
     }
   };
 
   const _columnElementPusher = (currentColumn, startRow, endRow) => {
-    console.log('============ ColumnPusher Called ============');
+    // console.log('============ ColumnPusher Called ============');
     // take each element from each row in column and push to result,
     // starting with rowTop and ending with rowBottom
     if (endRow > startRow) {
@@ -50,55 +61,77 @@ const snail = (array) => {
         // take row (which is i) and column( which is currentColumn)
         // to retreive specific element and push it to result
         result.push(array[i][currentColumn]);
-        console.log('COLUMN Top-Down Pushed Element: ', array[i][currentColumn]);
+        // console.log('COLUMN Top-Down Pushed Element: ', array[i][currentColumn]);
       }
     } else {
-      if (array.length % 2 === 0) {
-        for (let i = startRow; i >= endRow; i--) { // pushing from bottom row to top row
+      for (let i = startRow; i >= endRow; i--) { // pushing from bottom row to top row
         // take row (which is i) and column( which is currentColumn)
         // to retreive specific element and push it to result
-          result.push(array[i][currentColumn]);
-          console.log('COLUMN Bottom-Up Pushed Element: ', array[i][currentColumn]);
-        }
-      } else {
-        for (let i = endRow; i >= startRow; i--) { // pushing from bottom row to top row
-          // take row (which is i) and column( which is currentColumn)
-          // to retreive specific element and push it to result
-          result.push(array[i][currentColumn]);
-          console.log('COLUMN Bottom-Up Pushed Element: ', array[i][currentColumn]);
-        }
+        result.push(array[i][currentColumn]);
+        // console.log('COLUMN Bottom-Up Pushed Element: ', array[i][currentColumn]);
+      }
+      // } else {
+      //   for (let i = endRow; i >= startRow; i--) { // pushing from bottom row to top row
+      //     // take row (which is i) and column( which is currentColumn)
+      //     // to retreive specific element and push it to result
+      //     result.push(array[i][currentColumn]);
+      // console.log('COLUMN Bottom-Up Pushed Element: ', array[i][currentColumn]);
+      //   }
+    }
+  }
+
+  if (odd) {
+    while (rowBottom - rowTop >= 0) { // rowBottom - rowTop >= 0 && columnBack - columnFront >= 0
+      // Take Top Row and push to array, increment rowTop
+      _rowElementPusher(rowTop, columnFront, columnBack);
+      rowTop++;
+      // console.log('+++rowTop incremented to:', rowTop);
+      // add check to see if rowTop = rowBottom and colback = colFront
+      if (rowBottom > rowTop) {
+      // Take Back Column and push to array, decrement columnBack
+        _columnElementPusher(columnBack, rowTop, rowBottom);
+        columnBack--;
+        // console.log('---columnBack decremented to:', columnBack);
+      
+        // Take Bottom Row and push to array, decrement rowBottom
+        _rowElementPusher(rowBottom, columnBack, columnFront);
+        rowBottom--;
+        // console.log('---rowBottom decremented to:', rowBottom);
+      
+        // Take Front Column and push to array, increment columnFront
+        _columnElementPusher(columnFront, rowBottom, rowTop);   
+        columnFront++;
+        // console.log('+++columnFront incremented to: ', columnFront);
       }
     }
-  };
-  
-  // Check for empty input array
-  if (array.length === 1 || array.length === 0) return [[]];
+  }
 
-  // Check for NON square array
-  if (!(height === width)) return null;
-  
-  while (rowBottom - rowTop >= 0 && columnBack - columnFront >= 0) {
-    // Take Top Row and push to array, increment rowTop
-    _rowElementPusher(rowTop, columnFront, columnBack);
-    rowTop++;
-    console.log('+++rowTop incremented to:', rowTop);
-    // add check to see if rowTop = rowBottom and colback = colFront
-    if (rowBottom > rowTop) {
-    // Take Back Column and push to array, decrement columnBack
-      _columnElementPusher(columnBack, rowTop, rowBottom);
-      columnBack--;
-      console.log('---columnBack decremented to:', columnBack);
-    
+  if (!odd) {
+    while (rowBottom - rowTop >= 0) { // rowBottom - rowTop >= 0 && columnBack - columnFront >= 0
+      // Take Top Row and push to array, increment rowTop
+      _rowElementPusher(rowTop, columnFront, columnBack);
+      rowTop++;
+      // console.log('+++rowTop incremented to:', rowTop);
+      // add check to see if rowTop = rowBottom and colback = colFront
+      if (rowBottom > rowTop) {
+        // Take Back Column and push to array, decrement columnBack
+        _columnElementPusher(columnBack, rowTop, rowBottom);
+        columnBack--;
+        // console.log('---columnBack decremented to:', columnBack);
+      }
+
       // Take Bottom Row and push to array, decrement rowBottom
       _rowElementPusher(rowBottom, columnBack, columnFront);
       rowBottom--;
-      console.log('---rowBottom decremented to:', rowBottom);
-    
-      // Take Front Column and push to array, increment columnFront
-      _columnElementPusher(columnFront, rowBottom, rowTop);   
-      columnFront++;
-      console.log('+++columnFront incremented to: ', columnFront);
-    }    
+      // console.log('---rowBottom decremented to:', rowBottom);
+      
+      if (rowBottom > rowTop) {
+        // Take Front Column and push to array, increment columnFront
+        _columnElementPusher(columnFront, rowBottom, rowTop);
+        columnFront++;
+        // console.log('+++columnFront incremented to: ', columnFront);
+      }
+    }
   }
   return result;
 };
@@ -109,7 +142,6 @@ const array = [
   [4, 5, 6],
   [7, 8, 9],
 ];
-// console.log(snail(array));
 
 const arrayTwo = [
   [1, 2, 3, 4],
@@ -117,7 +149,6 @@ const arrayTwo = [
   [9, 10, 11, 12],
   [13, 14, 15, 16],
 ];
-// console.log(snail(arrayTwo));
 
 const arrayThree = [
   [1, 2, 3, 4, 5],
@@ -126,7 +157,10 @@ const arrayThree = [
   [16, 17, 18, 19, 20],
   [21, 22, 23, 24, 25],
 ];
-// console.log(snail(arrayThree));
+
+// console.log(snail(array));
+// console.log(snail(arrayTwo));
+// snail(arrayThree);
 
 // export default snail;
 module.exports = snail;
--- codewars/snailArray.test.js
+++ codewars/snailArray.test.js
@@ -21,13 +21,22 @@ const arrayThree = [
   [21, 22, 23, 24, 25],
 ];
 
+const arrayFour = [
+  [1, 2, 3, 4, 5, 6],
+  [7, 8, 9, 10, 11, 12],
+  [13, 14, 15, 16, 17, 18],
+  [19, 20, 21, 22, 23, 24],
+  [25, 26, 27, 28, 29, 30],
+  [31, 32, 33, 34, 35, 36],
+];
+
 describe('Solution', () => {
   it('Should Pass with basic 3x3 2d array', () => {
     expect(snail(array)).toEqual([1, 2, 3, 6, 9, 8, 7, 4, 5]);
   });
 
-  it('Should Pass with basic 4x3 2d array', () => {
-    expect(snail(arrayTwo)).toEqual([1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 5, 6, 7, 11, 10]);
+  it('Should Pass with basic 4x4 2d array', () => {
+    expect(snail(arrayTwo)).toEqual([1, 2, 3, 4, 8,12,16,15,14,13,9,5,6,7,11,10]);
   });
   
   it('Should Pass with complicated 5x5 array', () => {
@@ -36,8 +45,21 @@ describe('Solution', () => {
       21, 16, 11, 6, 7, 8, 9, 14, 19, 18, 17, 12, 13]);
   });
 
-  it('Should Pass with basic empty array', () => {
-    expect(snail([[]])).toEqual([[]]);
+  it('Should Pass with complicated 6x6 array', () => {
+    expect(snail(arrayFour)).toEqual([1, 2, 3, 4, 5, 6, 12, 18, 24, 30, 36, 35,
+      34, 33, 32, 31, 25, 19, 13, 7, 8, 9, 10, 11,
+      17, 23, 29, 28, 27, 26, 20, 14, 15, 16, 22, 21,
+    ]);
+  });
+
+  it('Should Return empty array with input of empty 2d array or empty array', () => {
+    expect(snail([[]])).toEqual([]);
+    expect(snail([[1]])).toEqual([1]);
+    expect(snail([])).toEqual([]);
+  });
+
+  it('Should return null is array is not square', () => {
+    expect(snail([[1,2,3],[4,5,6]])).toBeNull();
   });
 });
 
