20873dc19dc8c8803e05d32dcec0d76c8464862f
SethDonohue <donohue.seth@gmail.com>
2018-03-18 22:25:09 -0700

    passes 7x7 2d array test

--- codewars/snailArray.js
+++ codewars/snailArray.js
@@ -1,14 +1,12 @@
 // Given an n x n array, return the array elements
 // arranged from outermost elements to the middle
 // element, traveling clockwise.
-
 //   array = [[1, 2, 3],
 //           [4, 5, 6],
 //           [7, 8, 9]]
 // snail(array) #=> [1, 2, 3, 6, 9, 8, 7, 4, 5]
 
 const snail = (array) => {
-
   // Check for empty input array
   if (array.length === 1) return array[0];
   if (array.length === 0) return [];
@@ -20,17 +18,11 @@ const snail = (array) => {
   let rowBottom = array.length - 1; // decrement by 1 when finished with row
   let columnBack = array[0].length - 1; // decrement by 1 when finsihed with column
   let columnFront = 0; // increment by 1 when finished with column
-  // const height = array.length;
-  // const width = array[0].length;
-
-  let odd = true; // assume it is odd first
-  if (array.length % 2 === 0) odd = false; // array is even
-
-  // console.log(`Height:${height} Width:${width}`);
-  // console.log(`RowTop:${rowTop} RowBottom:${rowBottom}...ColumnBack:${columnBack} ColumnStart:${columnFront}`);
 
   const result = [];
-  // const map = {};
+  
+  let odd = true; // assume it is odd first
+  if (array.length % 2 === 0) odd = false; // array is even
 
   const _rowElementPusher = (currentRow, startColumn, endColumn) => {
     // console.log('============ RowPusher Called ============');
@@ -70,15 +62,8 @@ const snail = (array) => {
         result.push(array[i][currentColumn]);
         // console.log('COLUMN Bottom-Up Pushed Element: ', array[i][currentColumn]);
       }
-      // } else {
-      //   for (let i = endRow; i >= startRow; i--) { // pushing from bottom row to top row
-      //     // take row (which is i) and column( which is currentColumn)
-      //     // to retreive specific element and push it to result
-      //     result.push(array[i][currentColumn]);
-      // console.log('COLUMN Bottom-Up Pushed Element: ', array[i][currentColumn]);
-      //   }
     }
-  }
+  };
 
   if (odd) {
     while (rowBottom - rowTop >= 0) { // rowBottom - rowTop >= 0 && columnBack - columnFront >= 0
@@ -86,6 +71,7 @@ const snail = (array) => {
       _rowElementPusher(rowTop, columnFront, columnBack);
       rowTop++;
       // console.log('+++rowTop incremented to:', rowTop);
+
       // add check to see if rowTop = rowBottom and colback = colFront
       if (rowBottom > rowTop) {
       // Take Back Column and push to array, decrement columnBack
@@ -135,32 +121,4 @@ const snail = (array) => {
   }
   return result;
 };
-
-// test code
-const array = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-];
-
-const arrayTwo = [
-  [1, 2, 3, 4],
-  [5, 6, 7, 8],
-  [9, 10, 11, 12],
-  [13, 14, 15, 16],
-];
-
-const arrayThree = [
-  [1, 2, 3, 4, 5],
-  [6, 7, 8, 9, 10],
-  [11, 12, 13, 14, 15],
-  [16, 17, 18, 19, 20],
-  [21, 22, 23, 24, 25],
-];
-
-// console.log(snail(array));
-// console.log(snail(arrayTwo));
-// snail(arrayThree);
-
-// export default snail;
 module.exports = snail;
--- codewars/snailArray.test.js
+++ codewars/snailArray.test.js
@@ -30,13 +30,23 @@ const arrayFour = [
   [31, 32, 33, 34, 35, 36],
 ];
 
+const arrayFive = [
+  [1, 2, 3, 4, 5, 6, 7],
+  [8, 9, 10, 11, 12, 13, 14],
+  [15, 16, 17, 18, 19, 20, 21],
+  [22, 23, 24, 25, 26, 27, 28],
+  [29, 30, 31, 32, 33, 34, 35],
+  [36, 37, 38, 39, 40, 41, 42],
+  [43, 44, 45, 46, 47, 48, 49],
+];
+
 describe('Solution', () => {
   it('Should Pass with basic 3x3 2d array', () => {
     expect(snail(array)).toEqual([1, 2, 3, 6, 9, 8, 7, 4, 5]);
   });
 
   it('Should Pass with basic 4x4 2d array', () => {
-    expect(snail(arrayTwo)).toEqual([1, 2, 3, 4, 8,12,16,15,14,13,9,5,6,7,11,10]);
+    expect(snail(arrayTwo)).toEqual([1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10]);
   });
   
   it('Should Pass with complicated 5x5 array', () => {
@@ -52,6 +62,13 @@ describe('Solution', () => {
     ]);
   });
 
+  it('Should Pass with complicated 7x7 array', () => {
+    expect(snail(arrayFive)).toEqual([1, 2, 3, 4, 5, 6, 7, 14, 21, 28, 35, 42, 49, 48, 47,
+      46, 45, 44, 43, 36, 29, 22, 15, 8, 9, 10, 11,
+      12, 13, 20, 27, 34, 41, 40, 39, 38, 37, 30, 23,
+      16, 17, 18, 19, 26, 33, 32, 31, 24, 25]);
+  });
+
   it('Should Return empty array with input of empty 2d array or empty array', () => {
     expect(snail([[]])).toEqual([]);
     expect(snail([[1]])).toEqual([1]);
@@ -59,7 +76,7 @@ describe('Solution', () => {
   });
 
   it('Should return null is array is not square', () => {
-    expect(snail([[1,2,3],[4,5,6]])).toBeNull();
+    expect(snail([[1, 2, 3], [4, 5, 6]])).toBeNull();
   });
 });
 
