f55b44f2d15fe2d7d9a5d6c59ee4298c3d5adf5f
SethDonohue <donohue.seth@gmail.com>
2018-04-12 15:22:33 -0700

    added tests for climbleaderBaord

--- hackerrank/climbLeaderBoard.js
+++ hackerrank/climbLeaderBoard.js
@@ -1,27 +1,5 @@
 
 
-const fs = require('fs');
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', inputStdin => {
-  inputString += inputStdin;
-});
-
-process.stdin.on('end', _ => {
-  inputString = inputString.trim().split('\n').map(str => str.trim());
-
-  main();
-});
-
-function readLine() {
-  return inputString[currentLine++];
-}
-
 /*
  * Complete the climbingLeaderboard function below.
  */
@@ -33,6 +11,8 @@ function climbingLeaderboard(scores, alice) {
     
   // set j index outside of first loop so that it can be updated   
   let j = uniqueScores.length;
+  console.log('Ranks: ', j);
+  console.log('Played Games: ', alice.length);
     
   // for each of alice's scores
   for (let i = 0; i < alice.length; i++) {
@@ -45,12 +25,10 @@ function climbingLeaderboard(scores, alice) {
       // this loop is running in reverse along array
       while (j >= 0) {
         const rank = j + 1;
-        console.log('j & rank', j, rank);
         // if tied, push and break the loop
         if (alice[i] === uniqueScores[j]) {
           // set j to rank above this tied rank so we don't check the scores below this from now on
           j = rank - 1;
-          console.log('---- j updated to: ', j, ' ------');
           // console.log(j, rank);
           results.push(rank);
           break;
@@ -60,7 +38,6 @@ function climbingLeaderboard(scores, alice) {
         if (alice[i] < uniqueScores[j]) {
           // set j to rank so we don't check the scores below this from now on
           j = rank;
-          console.log('---- j updated to: ', j, ' ------');
           // console.log(j, rank);
           results.push(rank + 1);
           break;
@@ -72,20 +49,4 @@ function climbingLeaderboard(scores, alice) {
   return results;
 }
 
-function main() {
-  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-  const scoresCount = parseInt(readLine(), 10);
-
-  const scores = readLine().split(' ').map(scoresTemp => parseInt(scoresTemp, 10));
-
-  const aliceCount = parseInt(readLine(), 10);
-
-  const alice = readLine().split(' ').map(aliceTemp => parseInt(aliceTemp, 10));
-
-  const result = climbingLeaderboard(scores, alice);
-
-  ws.write(`${result.join("\n")  }\n`);
-
-  ws.end();
-}
+module.exports = climbingLeaderboard;
