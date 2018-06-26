83c034a78fe734c234ef0b7539bde554f3957f54
SethDonohue <donohue.seth@gmail.com>
2018-06-26 14:29:05 -0700

    partially finished

--- textio/top10MostRepeatedPhrases.js
+++ textio/top10MostRepeatedPhrases.js
@@ -14,3 +14,59 @@
 // Example output
 
 // ['the lazy dog', 'the quick brown fox jumped over']
+
+
+// find all the phrases in a sentence at least 3 words long
+const sentencePhraseFinder = (sentenceStr) => {
+  const sentenceArr = sentenceStr.split(' ');
+  const phraseMap = {};
+
+  for (let i = 0; i < sentenceArr.length - 2; i++) {
+    for (let j = i + 2; j < sentenceArr.length; j++) {
+      let tempStr = `${sentenceArr[i]} `;
+      for (let k = i + 1; k <= j; k++) {
+        tempStr = `${tempStr}${sentenceArr[k]} `;
+      }
+      if (phraseMap[tempStr]) {
+        phraseMap[tempStr]++;
+      } else {
+        phraseMap[tempStr] = 1;
+      }
+    }
+  }
+  return phraseMap;
+};
+
+const testStr = 'I am a big brown cow a big brown cow a big brown cow';
+const phrases = sentencePhraseFinder(testStr);
+
+// First remove non-repeated phrase
+const removeNonRepeats = (inputMap) => {
+  const newMap = {};
+  const keysArray = Object.keys(inputMap);
+
+  for (let i = 0; i < keysArray.length; i++) {
+    if (inputMap[keysArray[i]] > 1) {
+      newMap[keysArray[i]] = inputMap[keysArray[i]];
+    }
+  }
+  return newMap;
+};
+
+const cleanedMap = removeNonRepeats(phrases);
+
+// In the top 10 repeated phrases, remove any phrase that is INCLUDED in another longer phrase
+const cleanedMapKeysArr = Object.keys(cleanedMap);
+const result = [];
+console.log(cleanedMapKeysArr);
+for (let i = 0; i < cleanedMapKeysArr.length; i++) {
+  for (let j = 0; j < cleanedMapKeysArr.length; j++) {
+    console.log('I str: ', cleanedMapKeysArr[i]);
+    console.log('J str: ', cleanedMapKeysArr[j]);
+    if (!cleanedMapKeysArr[i].includes(cleanedMapKeysArr[j])) {
+      result.push(cleanedMapKeysArr[i]);
+    } 
+  }
+}
+
+// console.log('RESULT: ', result);
